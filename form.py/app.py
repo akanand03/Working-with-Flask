61c8fcd3500e19d355febdfca425465aedf70284
Akshit Anand <94638561+akanand03@users.noreply.github.com>
2024-07-09 20:48:24 +0530

    Create app.py

---
+++ form.py/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from forms import SignupForm, LoginForm
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "this_is_a_secret_key"
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title="Home")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash(f"Successfully Registered {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("signup.html", title="Sign Up", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        if email == "a@b.com" and password == "12345":
+            flash("Logged in Successfully!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Incorrect email or password", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
